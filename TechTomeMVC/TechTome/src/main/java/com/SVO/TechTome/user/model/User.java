b8215cca7b0827c8cd88a7340076abefaac60825
samuilovcharov <samuil.ovcharov01@gmail.com>
2025-03-11 18:15:33 +0200

    Update User class, service and repository

--- TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/user/model/User.java
+++ TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/user/model/User.java
@@ -1,5 +1,6 @@
 package com.SVO.TechTome.user.model;
 
+import com.SVO.TechTome.shoppingCart.model.ShoppingCart;
 import com.SVO.TechTome.subscription.model.Subscription;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -40,6 +41,10 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shopping_cart_id", referencedColumnName = "id")
+    private ShoppingCart shoppingCart;
 
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "owner")
     private List<Subscription> subscriptions = new ArrayList<>();
--- TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/user/service/UserService.java
+++ TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/user/service/UserService.java
@@ -1,7 +1,68 @@
 package com.SVO.TechTome.user.service;
 
+import com.SVO.TechTome.exception.DomainException;
+import com.SVO.TechTome.user.model.User;
+import com.SVO.TechTome.user.model.UserRole;
+import com.SVO.TechTome.user.repository.UserRepository;
+import com.SVO.TechTome.web.dto.LoginRequest;
+import com.SVO.TechTome.web.dto.RegisterRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User login(LoginRequest loginRequest) {
+
+        Optional<User> optionUser = userRepository.findByEmail(loginRequest.getEmail());
+        if (optionUser.isEmpty()) {
+            throw new DomainException("Email or password are incorrect.");
+        }
+
+        User user = optionUser.get();
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new DomainException("Username or password are incorrect.");
+        }
+
+        return user;
+    }
+
+    @Transactional
+    public User register(RegisterRequest registerRequest) {
+
+        Optional<User> optionUser = userRepository.findByEmail(registerRequest.getEmail());
+        if (optionUser.isPresent()) {
+            throw new DomainException("Username [%s] already exist.".formatted(registerRequest.getUsername()));
+        }
+
+        User user = userRepository.save(initializeUser(registerRequest));
+
+
+        log.info("Successfully create new user account for username [%s] and id [%s]".formatted(user.getUsername(), user.getId()));
+
+        return user;
+    }
+
+    private User initializeUser(RegisterRequest registerRequest) {
+        return User.builder()
+                .username(registerRequest.getUsername())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(UserRole.USER)
+                .build();
+    }
 }
