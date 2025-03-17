8eda133502e3847bd76a9375fd192b9cfb2d5f98
samuilovcharov <samuil.ovcharov01@gmail.com>
2025-03-17 23:33:34 +0200

    Update Config

--- TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/config/BeanConfig.java
+++ TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/config/BeanConfig.java
@@ -3,7 +3,6 @@ package com.SVO.TechTome.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -12,6 +11,6 @@ public class BeanConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
 
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
