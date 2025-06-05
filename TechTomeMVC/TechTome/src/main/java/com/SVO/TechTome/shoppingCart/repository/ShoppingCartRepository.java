7db21828937f636da6793b55639bc55fcf27ae28
samuilovcharov <samuil.ovcharov01@gmail.com>
2025-06-05 15:54:07 +0300

    Remove unnecessary files from project folder.

--- TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/shoppingCart/repository/ShoppingCartRepository.java
+++ TechTomeMVC/TechTome/src/main/java/com/SVO/TechTome/shoppingCart/repository/ShoppingCartRepository.java
@@ -1,7 +1,11 @@
 package com.SVO.TechTome.shoppingCart.repository;
 
+import com.SVO.TechTome.shoppingCart.model.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ShoppingCartRepository {
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, UUID> {
 }
